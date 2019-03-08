8abb841c1f4f122a35c315afd8cbb783cba04333
nicolasolo <41766950+nicolasolo@users.noreply.github.com>
2019-03-08 18:58:42 +0200

    Updates

---
+++ precache-manifest.40230a903cc8603632460c650bf288c9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "747b9a0f4cc2a44157eddcc964fcd6fb",
+    "url": "/WebApplication2/static/media/header-bg.747b9a0f.jpg"
+  },
+  {
+    "revision": "fda035692acfb328f67a",
+    "url": "/WebApplication2/static/js/runtime~main.fda03569.js"
+  },
+  {
+    "revision": "bc3168f016a9cbcbe0bb",
+    "url": "/WebApplication2/static/js/main.bc3168f0.chunk.js"
+  },
+  {
+    "revision": "9e474e0850e95dd471d2",
+    "url": "/WebApplication2/static/js/2.9e474e08.chunk.js"
+  },
+  {
+    "revision": "bc3168f016a9cbcbe0bb",
+    "url": "/WebApplication2/static/css/main.386490bd.chunk.css"
+  },
+  {
+    "revision": "c66307c65da0a83a766de50064481b57",
+    "url": "/WebApplication2/index.html"
+  }
+];
